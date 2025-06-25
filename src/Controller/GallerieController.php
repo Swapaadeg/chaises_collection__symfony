e134551be7401818b8e38721686b5792164761c7
Marie Rivier <marie.rivier23@gmail.com>
2025-06-25 14:08:22 +0200

    first commit, scss, variables

---
+++ src/Controller/GallerieController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class GallerieController extends AbstractController
+{
+    #[Route('/gallerie', name: 'app_gallerie')]
+    public function index(): Response
+    {
+        return $this->render('gallerie/index.html.twig', [
+            'controller_name' => 'GallerieController',
+        ]);
+    }
+}
